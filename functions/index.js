fce5af22572c03878bd32e7067677598f020293a
Tim Fong <timfong888@gmail.com>
2023-11-26 19:39:11 -0800

    update logs to get bitrate and remove multiple responses to a given request

--- functions/index.js
+++ functions/index.js
@@ -12,12 +12,14 @@ exports.videoIdToMP4 = functions.https.onRequest(async (req, res) => {
     const providedApiKey = req.headers['x-api-key'];
     if (providedApiKey !== apiKey) {
         res.status(401).send('Unauthorized');
+        responseSent = true;
         return;
     }
 
     // Check request method
     if (req.method !== 'POST') {
         res.status(405).send('Method Not Allowed');
+        responseSent = true;
         return;
     }
 
@@ -31,24 +33,21 @@ exports.videoIdToMP4 = functions.https.onRequest(async (req, res) => {
 
         if (audioFormats.length === 0) {
             res.status(404).send('No audio formats available for this video');
+            responseSent = true;
             return;
         }
 
-        // Select the audio format with the lowest bitrate
         const lowestQualityFormat = audioFormats.sort((a, b) => a.audioBitrate - b.audioBitrate)[0];
+        console.log(lowestQualityFormat);
+
         const fileExtension = lowestQualityFormat.container;
         const contentLength = lowestQualityFormat.contentLength;
+        const bitrate = lowestQualityFormat.audioBitrate; // Storing bitrate in case we need it later
         const fileName = `${videoId}.${fileExtension}`;
 
-        console.log(`Downloading audio from ${videoId} to ${fileName} of contentLength ${contentLength} via ${lowestQualityFormat.url}`);
-
-        // Reference to your Firebase Cloud Storage bucket
         const bucket = admin.storage().bucket();
-
-        // Create a file reference
         const file = bucket.file(fileName);
 
-        // Create a stream using ytdl-core
         const audioStream = ytdl(videoUrl, { quality: lowestQualityFormat.itag });
         const firebaseStream = file.createWriteStream({
             metadata: {
@@ -56,12 +55,11 @@ exports.videoIdToMP4 = functions.https.onRequest(async (req, res) => {
             },
         });
 
-        const TOTAL_LOG_LINES = 5;
-        let logInterval = contentLength // TOTAL_LOG_LINES;
+        let logInterval = contentLength/5; // TOTAL_LOG_LINES;
         let nextLogPoint = logInterval;
         let totalBytesReceived = 0;
+        let isStreamError = false;
 
-        // Add error handling and logging for the audio stream
         audioStream.on('data', (chunk) => {
             totalBytesReceived += chunk.length;
             if (totalBytesReceived >= nextLogPoint) {
@@ -70,6 +68,7 @@ exports.videoIdToMP4 = functions.https.onRequest(async (req, res) => {
             }
         }).on('error', (streamError) => {
             console.error(`Error in audio stream for ${videoId}:`, streamError);
+            isStreamError = true;
             if (!responseSent) {
                 res.status(500).send('Error in audio stream');
                 responseSent = true;
@@ -81,54 +80,42 @@ exports.videoIdToMP4 = functions.https.onRequest(async (req, res) => {
                 console.log('Audio stream ended successfully.');
             }
         });
-        
+
         firebaseStream.on('error', (err) => {
             console.error('Error uploading to Firebase Cloud Storage', err);
+            isStreamError = true;
             if (!responseSent) {
                 res.status(500).send('Error uploading audio');
                 responseSent = true;
             }
         });
 
-        let isStreamError = false;
-
-        audioStream.on('error', (streamError) => {
-            console.error(`Error in audio stream for ${videoId}:`, streamError);
-            isStreamError = true;
-            res.status(500).send('Error in audio stream');
-        });
-
-        firebaseStream.on('error', (err) => {
-            console.error('Error uploading to Firebase Cloud Storage', err);
-            isStreamError = true;
-            res.status(500).send('Error uploading audio');
-        });
-
-        // Pipe the ytdl-core stream to Firebase Cloud Storage
         audioStream.pipe(firebaseStream)
-            .on('error', (err) => {
-                console.error('Error uploading to Firebase Cloud Storage', err);
-                res.status(500).send('Error uploading audio');
-            })
             .on('finish', async () => {
-                console.log('Upload complete');
+                if (isStreamError) {
+                    return;
+                }
+                console.log('Upload complete:', fileName  );
+
+                // Consolidated log statement for file details
+                console.log(`File Details:
+                    Name: ${metadata[0].name}
+                    Bucket: ${metadata[0].bucket}
+                    Size: ${metadata[0].size} bytes
+                    MD5 Hash: ${metadata[0].md5Hash}
+                    Content Type: ${metadata[0].contentType}
+                    Created: ${metadata[0].timeCreated}
+                    Updated: ${metadata[0].updated}
+                    Bitrate: ${bitrate} kbps`);
+
                 try {
                     await file.makePublic();
                     const publicUrl = file.publicUrl();
-                    console.log('File made public at URL:', publicUrl);
 
-                    // Fetch file metadata to confirm file size
                     const metadata = await file.getMetadata();
-                    console.log(`Uploaded file metadata for ${videoId}: 
-                                Name: ${metadata[0].name}, 
-                                Bucket: ${metadata[0].bucket}, 
-                                Size: ${metadata[0].size} bytes, 
-                                MD5 Hash: ${metadata[0].md5Hash}, 
-                                Content Type: ${metadata[0].contentType}, 
-                                Created: ${metadata[0].timeCreated}, 
-                                Updated: ${metadata[0].updated}, 
-                                Media Link: ${metadata[0].mediaLink}`);
 
+                    console.log()
+                    
                     if (!responseSent) {
                         res.status(200).send({
                             audio_url: publicUrl,
@@ -137,14 +124,21 @@ exports.videoIdToMP4 = functions.https.onRequest(async (req, res) => {
                             file_size: metadata[0].size
                         });
                         responseSent = true;
-                    };
+                    }
                 } catch (publicError) {
                     console.error('Error making file public:', publicError);
-                    res.status(500).send('Error making file public');
+                    if (!responseSent) {
+                        res.status(500).send('Error making file public');  
+                        responseSent = true; 
+                    }
                 }
             });
+
     } catch (error) {
         console.error(`Error in YouTube audio extraction function: `, error);
-        res.status(500).send('Internal Server Error');
+        if (!responseSent) {
+            res.status(500).send('Internal Server Error');
+            responseSent = true;
+        }
     }
 });
